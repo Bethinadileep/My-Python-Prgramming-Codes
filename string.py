897d3af493da58ee5a4a8e28d2a02fdf71d44064
Bethina Dileep <56349269+Bethinadileep@users.noreply.github.com>
2021-11-17 17:33:26 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,6 @@
+#codeby : Dileep
+mystr = "           Welcome to ALl         "
+print(mystr,len(mystr))
+print(mystr.strip(),len(mystr.strip())) #Strip removes unwanted empty space
+print(mystr.lstrip(),len(mystr.lstrip()))
+print(mystr.rstrip(),len(mystr.rstrip()))
